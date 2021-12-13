cca4844fa99cd70ccde501d817045e39eca3497a
David <emekachinye09@gmail.com>
2021-12-13 20:08:31 +0100

    structured the display of the section

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,10 +3,20 @@ import styled from 'styled-components'
 
 function Section() {
     return (
-        <Wrap>
-            Section
-        </Wrap>
-    )
+      <Wrap>
+        <ItemText>
+          <h1>Model S</h1>
+          <p>Order Online for Touchless Delivery</p>
+        </ItemText>
+        <Buttons>
+          <ButtonGroup>
+            <LeftButton>Custom Order</LeftButton>
+            <RightButton>Existing Inventory</RightButton>
+          </ButtonGroup>
+          <DownArrow src="/images/down-arrow.svg" />
+        </Buttons>
+      </Wrap>
+    );
 }
 
 export default Section
@@ -14,5 +24,50 @@ export default Section
 const Wrap = styled.div`
     width: 100vw;
     height: 100vh;
-    background: orange;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url('/images/model-s.jpg');
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between; //for vertical alignment
+    align-items: center; // for horizontal alignment
+`
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+`
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+    margin: 8px;
+    
+`
+const RightButton = styled(LeftButton)`
+    background: white;
+    opacity: 0.65;
+    color: black;
+`
+const DownArrow = styled.img`
+    //margin-top: 20px;
+    height: 40px;
+    overflow-x: hidden;
+    animation: animateDown infinite 1.5s;
+`
+const Buttons = styled.div`
+
 `
