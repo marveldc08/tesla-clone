3b0bb5f7b68cbc3b9a0f454abb2f615319e91572
David <emekachinye09@gmail.com>
2021-12-13 17:59:51 +0100

    Added Wrap style to Section component

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Section() {
     return (
-        <div>
+        <Wrap>
             Section
-        </div>
+        </Wrap>
     )
 }
 
 export default Section
+
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: orange;
+`
