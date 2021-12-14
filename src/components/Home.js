ae122c9ddd5c8ebf2252d46b0621eb82fc780ba9
David <emekachinye09@gmail.com>
2021-12-14 11:37:10 +0100

    input dynamic content into section by props

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,10 +7,48 @@ import Section from './Section'
 function Home() {
     return (
       <Container>
-        <Section />
-        <Section />
-        <Section />
-        <Section />
+        <Section
+          title="Model S"
+          description="Order Online for Touchless Delivery"
+          backgroundImage="model-s.jpg"
+          leftBtnText="Custom Order"
+          rightBtnText="Existing inventory"
+        />
+        <Section
+          title="Model Y"
+          description="Order Online for Touchless Delivery"
+          backgroundImage="model-y.jpg"
+          leftBtnText="Custom Order"
+          rightBtnText="Existing inventory"
+        />
+        <Section
+          title="Model 3"
+          description="Order Online for Tochless Delivery"
+          backgroundImage="model-3.jpg"
+          leftBtnText="Custom Order"
+          rightBtnText="Existing inventory"
+        />
+        <Section
+          title="Model X"
+          description="Order Online for Tochless Delivery"
+          backgroundImage="model-x.jpg"
+          leftBtnText="Custom Order"
+          rightBtnText="Existing inventory"
+        />
+        <Section
+          title="Lowest Cost Solar Panels in America"
+          description="Money-back Guarantee"
+          backgroundImage="solar-panel.jpg"
+          leftBtnText="Order Now"
+          rightBtnText="Learn More"
+        />
+        <Section
+          title="Accessories"
+          description=""
+          backgroundImage="accessories.jpg"
+          leftBtnText="Shop Now"
+        />
+        
       </Container>
     );
 }
